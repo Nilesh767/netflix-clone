fe526c384511a5790fc9e7b5fc19f2a73ee58358
Nilesh Choudhary <nilesh.choudhary797@gmail.com>
2021-02-11 15:42:37 +0530

    Added Homescreen

---
+++ src/Firebase/firebase.js
@@ -0,0 +1,8 @@
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBX4hVz_ipa_DZk3TuJyqhE1gknIQ1b8KY",
+//   authDomain: "netflix-clone-neo.firebaseapp.com",
+//   projectId: "netflix-clone-neo",
+//   storageBucket: "netflix-clone-neo.appspot.com",
+//   messagingSenderId: "240397869368",
+//   appId: "1:240397869368:web:ac959e42d4d9004f1521cd",
+// };
---
+++ src/Requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "a417cf3c30f748d4ec50670a5ba72b37";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-us`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-us`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
